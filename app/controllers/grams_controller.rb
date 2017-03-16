fa9cb10a89102ca30150bfdfb39cd2e303e61901
Anna Canelas <canelasa2@gmail.com>
2017-03-16 15:59:17 +0000

    Edit and update posts

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -6,15 +6,30 @@ class GramsController < ApplicationController
 
   def show
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render plain: 'Not Found :(', status: :not_found
-    end
+    return render_not_found if @gram.blank?
   end
 
   def new
     @gram = Gram.new
   end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+  
   def create
     @gram = Gram.create(gram_params.merge(user: current_user))
     if @gram.valid?
@@ -28,5 +43,9 @@ class GramsController < ApplicationController
 
   def gram_params
     params.require(:gram).permit(:message)
+  end
+
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
   end
 end
